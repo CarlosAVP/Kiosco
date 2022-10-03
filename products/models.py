7c64bf28ccc8653457787cc837b302f52fe9e89e
Carlos Vieira <carlosalberovp96@gmail.com>
2022-10-03 19:21:59 -0300

    ignorar la base de datos, migraciones

--- products/models.py
+++ products/models.py
@@ -7,14 +7,12 @@ class Product(models.Model):
   price = models.FloatField()
   stock = models.IntegerField()
   barcode = models.CharField(max_length=13)
-  stock_orders = models.ManyToManyField('StockOrder', through='StockOrderForProduct')
 
   def __str__(self):
     return self.name
 
 class StockOrder(models.Model):
   products = models.ManyToManyField(Product, through='StockOrderForProduct')
-
 
 class StockOrderForProduct(models.Model):
   stock_order = models.ForeignKey(StockOrder, on_delete=models.CASCADE)
